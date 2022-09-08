6f0999bd5b043855a4600821046fd7a069bd3f5d
unknown <zarzaedu@gmail.com>
2022-09-08 16:34:41 -0300

    Al fin pude subirlo

--- Familis/models.py
+++ Familis/models.py
@@ -1,3 +1,11 @@
 from django.db import models
+from datetime import date
+
+
+class Persons (models.model):
+    First_name = models.CharField(max_length=28 )
+    Last_name = models.CharField(max_length=28)
+    Date_of_birth = models.DateField()
+    Sons = models.IntegerField()
 
 # Create your models here.
